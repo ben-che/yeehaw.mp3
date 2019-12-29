b8bf11be89e5f3743d4001379a8a5a91854687d4
benche <ben.che9@gmail.com>
2019-12-29 13:57:25 -0500

    webpack additions, fixing flask stuff

---
+++ views/src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const rootElement = document.getElementById('app');
+
+render(<div> Hello World! </div>, rootElement);
--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: path.resolve(__dirname, 'app.js'),
+	entry: path.resolve(__dirname, 'views/src/app.js'),
 	output: {
 		path: path.resolve(__dirname, 'views/public'),
 		filename: 'bundle.js'
 	},
 	module: {
-		rules: [{
-			test: /\.js$/,
-			include: path.resolve(__dirname, 'src'),
-			use: ['babel-loader']
-		}]
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, 'views/src'),
+				use: ['babel-loader']
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader']
+			}
+		]
 	},
 	devServer: {
-		contentBase:  path.resolve(__dirname, 'dist'),
+		contentBase: path.resolve(__dirname, 'dist'),
 		port: 9000
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-	    	template: "src/index.html" //source html
+			template: './views/index.html' //source html
 		})
 	]
 };
